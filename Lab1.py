f6a8eeb2b0506b27faf3b429ec0c5feef10bdd35
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-02-17 21:37:01 +0200

    add class FiniteAutomaton

--- Lab1.py
+++ Lab1.py
@@ -12,3 +12,54 @@ class Grammar:
         }
         self.generated_strings = set()
 
+    def generateString(self):
+        def generateFromSymbol(symbol):
+            if symbol in self.VT:
+                return symbol
+            else:
+                production = random.choice(self.P[symbol])
+                return ''.join(generateFromSymbol(s) for s in production)
+
+        generated_word = generateFromSymbol(self.S)
+        while generated_word in self.generated_strings:  # Ensure uniqueness
+            generated_word = generateFromSymbol(self.S)
+        self.generated_strings.add(generated_word)  # Add to the set of generated strings
+        return generated_word
+
+    def generate5Words(self):
+        for _ in range(5):
+            w = self.generateString()
+            print("Generated string:", w)
+
+    def toFiniteAutomaton(self):
+        return FiniteAutomaton()
+
+class FiniteAutomaton:
+    def __init__(self):
+        self.states = {'S', 'A', 'B', 'C'}  # Set of states
+        self.alphabet = {'a', 'b', 'c', 'd'}  # Set of input symbols
+        self.transitions = {
+            ('S', 'd'): 'A',
+            ('A', 'd'): 'A',
+            ('A', 'a'): 'B',
+            ('B', 'b'): 'C',
+            ('C', 'c'): 'A',
+            ('C', 'a'): 'S'
+        }  # Dictionary representing transition function
+        self.initial_state = 'S'  # Initial state
+        self.accepting_states = {'S', 'A'}  # Set of accepting states
+
+    def accepts_string(self, input_string):
+        current_state = self.initial_state
+        for symbol in input_string:
+             if (current_state, symbol) in self.transitions:
+                current_state = self.transitions[(current_state, symbol)]
+             else:
+                 return False
+        return current_state in self.accepting_states
+
+    # Create an instance of the Grammar class
+grammar = Grammar()
+
+grammar.generate5Words()
+
