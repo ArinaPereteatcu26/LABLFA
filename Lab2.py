73d51e11437d735c5068b520303f1fec984874f3
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-03-03 16:25:17 +0200

    add method to convert NFA to DFA

--- Lab2.py
+++ Lab2.py
@@ -31,3 +31,34 @@ class FiniteAutomaton:
             if len(value) > 1:
                 return False
         return True
+
+    def nfa_to_dfa(self):
+        input_symbols = self.Sigma
+        initial_state = self.q0
+        states = []
+        final_states = self.F
+
+        transitions = {}
+        new_states = [initial_state]
+        while new_states:
+            for state in new_states:
+                new_states.remove(state)
+                if state not in transitions.keys():
+                    transitions[state] = {}
+                    for el in input_symbols:
+                        next_states = set()
+                        for s in state.split(','):
+                            if (s, el) in self.Delta.keys():
+                                next_states.update(self.Delta[(s, el)])
+                        next_states = ','.join(sorted(list(next_states)))
+                        transitions[state][el] = next_states
+                        if next_states not in transitions.keys() and next_states != '':
+                            new_states.append(next_states)
+
+        states = list(transitions.keys())
+
+        print(f"Q = {states}")
+        print(f"Sigma = {input_symbols}")
+        print(f"Delta = {transitions}")
+        print(f"q0 = {initial_state}")
+        print(f"F = {final_states}")
