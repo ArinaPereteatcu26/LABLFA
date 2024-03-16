703f08eb5001bf79a027298a612d8527a294a5fe
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-03-16 16:50:17 +0200

    add error handling

--- Lab3_LFA.py
+++ Lab3_LFA.py
@@ -21,6 +21,13 @@ t_NUMBER = r'[0-9]+'
 # Ignored characters (whitespace)
 t_ignore = ' \t\r\n'
 
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
 
 
 
